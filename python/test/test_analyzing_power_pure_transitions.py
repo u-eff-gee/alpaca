880e18415ba1f9878fe3e49654e0fe8c58389584
Udo Friman-Gayer <ufg@email.unc.edu>
2021-03-23 13:09:17 -0400

    Uniform analyzing-power plots
    - A(45) on x axis like in analyzing-power plots for single mixing ratio

--- python/test/test_analyzing_power_pure_transitions.py
+++ python/test/test_analyzing_power_pure_transitions.py
@@ -24,73 +24,76 @@ from alpaca.transition import ELECTRIC, MAGNETIC, Transition
 
 from alpaca.analyzing_power import AnalyzingPower
 
-spins = [2, 4, 6]
-spin_colors = ["black", "grey", "orange"]
-parity_symbols = ["x", "o"]
-analyzing_powers = []
 
-for two_J in spins:
-    em = ELECTRIC if two_J % 4 == 0 else MAGNETIC
-    emp = ELECTRIC if em == MAGNETIC else MAGNETIC
+def test_analyzing_power_pure_transitions():
+    spins = [2, 4, 6]
+    spin_markers = ["o", "s", "^"]
+    parity_colors = ["crimson", "royalblue"]
+    analyzing_powers = []
 
-    analyzing_powers.append(
-        AnalyzingPower(
-            AngularCorrelation(
-                State(0, POSITIVE),
-                [
+    for two_J in spins:
+        em = ELECTRIC if two_J % 4 == 0 else MAGNETIC
+        emp = ELECTRIC if em == MAGNETIC else MAGNETIC
+
+        analyzing_powers.append(
+            AnalyzingPower(
+                AngularCorrelation(
+                    State(0, POSITIVE),
                     [
-                        Transition(em, two_J, emp, two_J + 2, 0.0),
-                        State(two_J, POSITIVE),
+                        [
+                            Transition(em, two_J, emp, two_J + 2, 0.0),
+                            State(two_J, POSITIVE),
+                        ],
+                        [
+                            Transition(em, two_J, emp, two_J + 2, 0.0),
+                            State(0, POSITIVE),
+                        ],
                     ],
-                    [
-                        Transition(em, two_J, emp, two_J + 2, 0.0),
-                        State(0, POSITIVE),
-                    ],
-                ],
-            ),
-            convention="KPZ",
+                ),
+                convention="KPZ",
+            )
         )
-    )
 
-    em = ELECTRIC if two_J % 4 != 0 else MAGNETIC
-    emp = ELECTRIC if em == MAGNETIC else MAGNETIC
+        em = ELECTRIC if two_J % 4 != 0 else MAGNETIC
+        emp = ELECTRIC if em == MAGNETIC else MAGNETIC
 
-    analyzing_powers.append(
-        AnalyzingPower(
-            AngularCorrelation(
-                State(0, POSITIVE),
-                [
+        analyzing_powers.append(
+            AnalyzingPower(
+                AngularCorrelation(
+                    State(0, POSITIVE),
                     [
-                        Transition(em, two_J, emp, two_J + 2, 0.0),
-                        State(two_J, NEGATIVE),
+                        [
+                            Transition(em, two_J, emp, two_J + 2, 0.0),
+                            State(two_J, NEGATIVE),
+                        ],
+                        [
+                            Transition(em, two_J, emp, two_J + 2, 0.0),
+                            State(0, POSITIVE),
+                        ],
                     ],
-                    [
-                        Transition(em, two_J, emp, two_J + 2, 0.0),
-                        State(0, POSITIVE),
-                    ],
-                ],
-            ),
-            convention="KPZ",
+                ),
+                convention="KPZ",
+            )
         )
-    )
 
-_fontsize_axis_label = 13
-_xy_max = 1.2
+    _fontsize_axis_label = 13
+    _xy_max = 1.5
 
-fig, ax = plt.subplots(1, 1, figsize=(4.5, 4))
-ax.set_xlabel(r"$A(\theta = 90^\circ)$", fontsize=_fontsize_axis_label)
-ax.set_xlim(-_xy_max, _xy_max)
-ax.set_ylabel(r"$A(\theta = 45^\circ)$", fontsize=_fontsize_axis_label)
-ax.set_ylim(-_xy_max, _xy_max)
-for i, ana in enumerate(analyzing_powers):
-    ax.plot(
-        [ana(0.5 * np.pi)],
-        [ana(0.25 * np.pi)],
-        parity_symbols[0] if i % 2 == 0 else parity_symbols[1],
-        color=spin_colors[i // 2],
-        markersize=10,
-        label=ana.angular_correlation.cascade_steps[0][1].tex(),
-    )
-ax.legend()
-plt.tight_layout()
-plt.savefig("analyzing_power_0_123_0.pdf")
+    fig, ax = plt.subplots(1, 1, figsize=(4.5, 4))
+    ax.set_xlabel(r"$A(\theta = 45^\circ)$", fontsize=_fontsize_axis_label)
+    ax.set_xlim(-_xy_max, _xy_max)
+    ax.set_xticks([-1, -0.5, 0.0, 0.5, 1.0])
+    ax.set_ylabel(r"$A(\theta = 90^\circ)$", fontsize=_fontsize_axis_label)
+    ax.set_ylim(-_xy_max, _xy_max)
+    for i, ana in enumerate(analyzing_powers):
+        ax.plot(
+            [ana(0.25 * np.pi)],
+            [ana(0.5 * np.pi)],
+            spin_markers[i // 2],
+            color=parity_colors[0] if i % 2 == 0 else parity_colors[1],
+            markersize=8,
+            label=ana.angular_correlation.cascade_steps[0][1].tex(),
+        )
+    ax.legend()
+    plt.tight_layout()
+    plt.savefig("analyzing_power_0_123_0.pdf")
