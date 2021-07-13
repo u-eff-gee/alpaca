998dd242e1619a3ac20e6625801f1fa80d4b06bb
Udo Friman-Gayer <ufg@email.unc.edu>
2021-07-13 17:30:01 -0400

    Load all modules in __init__.py

--- python/alpaca/__init__.py
+++ python/alpaca/__init__.py
@@ -1,4 +1,6 @@
 from .analyzing_power import *
 from .angular_correlation import *
+from .inversion_by_grid_evaluation import *
+from .inversion_by_piecewise_interpolation import *
 from .state import *
 from .transition import *
