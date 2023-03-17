4f9ea101d91f206f1980b9f9d079b3b236951c70
Udo Friman-Gayer <ufrimangayer@gmail.com>
2023-03-17 17:32:25 +0100

    Refactor and extend Euler-angle tests
    - Loop over test configurations instead of copy-pasting similar code.
    - Verify self consistency.
    - Demonstrate ambiguity of tests (and show that it doesn't matter w.r.t. the final result)
    - Memory cleanup.
    - Adapt test_numerical_equality function further to this specific application.

--- test/test_euler_angle_rotation.cc
+++ test/test_euler_angle_rotation.cc
@@ -19,24 +19,99 @@
 
 #include <array>
 
+#include <iostream>
+
+using std::exception;
+
+#include <stdexcept>
+
+using std::runtime_error;
+
+#include <sstream>
+
+using std::stringstream;
+
+#include <string>
+
+using std::string;
+
+#include <vector>
+
+using std::vector;
+
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_math.h>
 
 #include "EulerAngleRotation.hh"
 #include "TestUtilities.hh"
 
-void test_numerical_equality(const size_t n, gsl_vector *a, gsl_vector *b,
+void test_numerical_equality(gsl_vector *a, gsl_vector *b,
                              const double epsilon) {
-  for (size_t i = 0; i < n; ++i) {
-    test_numerical_equality<double>(gsl_vector_get(a, i), gsl_vector_get(b, i),
-                                    epsilon);
+  for (size_t i = 0; i < 3; ++i) {
+    try {
+      test_numerical_equality<double>(gsl_vector_get(a, i),
+                                      gsl_vector_get(b, i), epsilon);
+    } catch (const exception &e) {
+      stringstream error_message;
+      error_message << e.what();
+      error_message << "For index " << i << " of vectors"
+                    << "\n";
+      error_message << "(" << std::scientific << gsl_vector_get(a, 0) << ", "
+                    << gsl_vector_get(a, 1) << ", " << gsl_vector_get(a, 2)
+                    << ")\n";
+      error_message << "(" << std::scientific << gsl_vector_get(b, 0) << ", "
+                    << gsl_vector_get(b, 1) << ", " << gsl_vector_get(b, 2)
+                    << ")\n";
+      throw runtime_error(error_message.str());
+    }
   }
 }
+
+struct RotationTest {
+  string name;
+  array<double, 3> initial_axis;
+  array<double, 3> target_axis;
+  array<double, 3> Phi_Theta_Psi;
+  array<double, 3> Phi_Theta_Psi_alternative;
+};
+
+const vector<RotationTest> rotation_tests{
+    // If Phi_Theta_Psi_alternative is initialized with zeros only, that means
+    // it won't be used.
+    RotationTest{"x_to_y", {1, 0, 0}, {0, 1, 0}, {M_PI_2, 0, 0}, {0., 0., 0.}},
+    RotationTest{
+        "x_to_z", {1, 0, 0}, {0, 0, 1}, {M_PI_2, M_PI_2, 0}, {0., 0., 0.}},
+    RotationTest{"y_to_x", {0, 1, 0}, {1, 0, 0}, {-M_PI_2, 0, 0}, {0., 0., 0.}},
+    RotationTest{
+        "y_to_z", {0, 1, 0}, {0, 0, 1}, {M_PI, -M_PI_2, M_PI}, {0, M_PI_2, 0}},
+    RotationTest{"z_to_x",
+                 {0, 0, 1},
+                 {1, 0, 0},
+                 {0, -M_PI_2, -M_PI_2},
+                 {-M_PI, M_PI_2, M_PI_2}},
+    RotationTest{"z_to_y",
+                 {0, 0, 1},
+                 {0, 1, 0},
+                 {0, -M_PI_2, 0},
+                 {-M_PI, M_PI_2, -M_PI}},
+    RotationTest{"x_to_x", {1, 0, 0}, {1, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+};
 
 /**
  * \brief Test rotations of 3D vectors using Euler angles.
  *
- * Test by rotating the three canonical Cartesian axes into each other.
+ * This program employs user-defined sets of Euler angles (1) to transform the
+ * primitive x, y, and z axes into each other (and one case where no
+ * transformation is necessary). After that the backward transformation is used
+ * to restore the initial vector (2). In a third step (3), the rotation matrix
+ * for the transformation in (1) is explicitly calculated, and it is
+ * demonstrated how to infer the Euler angles from it. Since the reverse
+ * engineering of Euler angles is not unique, the user-defined Euler angles may
+ * provide an alternative set of angles (knowing what will be the result of
+ * euler_angle_transform::angles) to achieve the same transformation. This
+ * ambiguity is handled by a try-catch clause (4). At least, it is verified that
+ * the original and alternative sets of Euler angles indeed describe the same
+ * transformation by applying it too all three primitive axes (5).
  */
 int main() {
 
@@ -44,94 +119,116 @@ int main() {
 
   gsl_vector *x_axis = gsl_vector_alloc(3);
   gsl_vector_set_basis(x_axis, 0);
+  gsl_vector *x_axis_transformed = gsl_vector_alloc(3);
+  gsl_vector *x_axis_transformed_from_reconstructed_matrix =
+      gsl_vector_alloc(3);
   gsl_vector *y_axis = gsl_vector_alloc(3);
   gsl_vector_set_basis(y_axis, 1);
+  gsl_vector *y_axis_transformed = gsl_vector_alloc(3);
+  gsl_vector *y_axis_transformed_from_reconstructed_matrix =
+      gsl_vector_alloc(3);
   gsl_vector *z_axis = gsl_vector_alloc(3);
   gsl_vector_set_basis(z_axis, 2);
+  gsl_vector *z_axis_transformed = gsl_vector_alloc(3);
+  gsl_vector *z_axis_transformed_from_reconstructed_matrix =
+      gsl_vector_alloc(3);
 
-  // Rotate x axis into y axis
-  gsl_vector *Phi_Theta_Psi_x_to_y = gsl_vector_alloc(3);
-  gsl_vector_set_zero(Phi_Theta_Psi_x_to_y);
-  gsl_vector_set(Phi_Theta_Psi_x_to_y, 0, -M_PI_2);
+  gsl_vector *initial_axis = gsl_vector_alloc(3);
+  gsl_vector *Phi_Theta_Psi = gsl_vector_alloc(3);
+  gsl_vector *initial_axis_transformed = gsl_vector_alloc(3);
+  gsl_vector *initial_axis_reconstructed = gsl_vector_alloc(3);
+  gsl_vector *target_axis = gsl_vector_alloc(3);
+  gsl_matrix *transformation_matrix = gsl_matrix_alloc(3, 3);
+  gsl_vector *Phi_Theta_Psi_reconstructed = gsl_vector_alloc(3);
+  gsl_vector *Phi_Theta_Psi_alternative = gsl_vector_alloc(3);
 
-  gsl_vector *x_rotated_to_y = gsl_vector_alloc(3);
-  euler_angle_transform::rotate(x_rotated_to_y, Phi_Theta_Psi_x_to_y, x_axis);
-  test_numerical_equality(3, x_rotated_to_y, y_axis, epsilon);
+  for (auto test : rotation_tests) {
+    gsl_vector_set(initial_axis, 0, test.initial_axis[0]);
+    gsl_vector_set(initial_axis, 1, test.initial_axis[1]);
+    gsl_vector_set(initial_axis, 2, test.initial_axis[2]);
 
-  gsl_vector *x_rotated_to_y_reversed = gsl_vector_alloc(3);
-  euler_angle_transform::rotate_back(x_rotated_to_y_reversed,
-                                     Phi_Theta_Psi_x_to_y, x_rotated_to_y);
-  test_numerical_equality(3, x_rotated_to_y_reversed, x_axis, epsilon);
+    gsl_vector_set(target_axis, 0, test.target_axis[0]);
+    gsl_vector_set(target_axis, 1, test.target_axis[1]);
+    gsl_vector_set(target_axis, 2, test.target_axis[2]);
 
-  // Rotate x axis into z axis
-  gsl_vector *Phi_Theta_Psi_x_to_z = gsl_vector_alloc(3);
-  gsl_vector_set_zero(Phi_Theta_Psi_x_to_z);
-  gsl_vector_set(Phi_Theta_Psi_x_to_z, 0, M_PI_2);
-  gsl_vector_set(Phi_Theta_Psi_x_to_z, 1, M_PI_2);
+    gsl_vector_set(Phi_Theta_Psi, 0, test.Phi_Theta_Psi[0]);
+    gsl_vector_set(Phi_Theta_Psi, 1, test.Phi_Theta_Psi[1]);
+    gsl_vector_set(Phi_Theta_Psi, 2, test.Phi_Theta_Psi[2]);
 
-  gsl_vector *x_rotated_to_z = gsl_vector_alloc(3);
-  euler_angle_transform::rotate(x_rotated_to_z, Phi_Theta_Psi_x_to_z, x_axis);
-  test_numerical_equality(3, x_rotated_to_z, z_axis, epsilon);
+    // (1) Verify that the given transformation turns the given initial axis
+    // into the given target axis.
+    euler_angle_transform::rotate(initial_axis_transformed, Phi_Theta_Psi,
+                                  initial_axis);
+    test_numerical_equality(initial_axis_transformed, target_axis, epsilon);
 
-  gsl_vector *x_rotated_to_z_reversed = gsl_vector_alloc(3);
-  euler_angle_transform::rotate_back(x_rotated_to_z_reversed,
-                                     Phi_Theta_Psi_x_to_z, x_rotated_to_z);
-  test_numerical_equality(3, x_rotated_to_z_reversed, x_axis, epsilon);
+    // (2) Verify that the euler_angle_transform::rotate_back function does
+    // exactly the opposite of euler_angle_transform::rotate.
+    euler_angle_transform::rotate_back(initial_axis_reconstructed,
+                                       Phi_Theta_Psi, initial_axis_transformed);
+    test_numerical_equality(initial_axis_reconstructed, initial_axis, epsilon);
 
-  // Rotate y axis into x axis
-  gsl_vector *Phi_Theta_Psi_y_to_x = gsl_vector_alloc(3);
-  gsl_vector_set_zero(Phi_Theta_Psi_y_to_x);
-  gsl_vector_set(Phi_Theta_Psi_y_to_x, 0, M_PI_2);
+    // (3) Obtain forward-transformation matrix and reconstruct the Euler angles
+    // from it.
+    euler_angle_transform::rotation_matrix(transformation_matrix,
+                                           Phi_Theta_Psi);
+    euler_angle_transform::angles(Phi_Theta_Psi_reconstructed,
+                                  transformation_matrix);
+    try {
+      // (4) Check whether the reconstructed Euler angles are the same as the
+      // original ones. ...
+      test_numerical_equality(Phi_Theta_Psi_reconstructed, Phi_Theta_Psi,
+                              epsilon);
+    } catch (exception &e) {
+      // (4) ... Sometimes, this is not the case, because the transformations
+      // are ambiguous. For this reason, the test cases contain an alternative
+      // set of Euler angles. The alternative set was found -by trial and error-
+      // to be the one that is really returned by euler_angle_transform::angles.
+      gsl_vector_set(Phi_Theta_Psi_alternative, 0,
+                     test.Phi_Theta_Psi_alternative[0]);
+      gsl_vector_set(Phi_Theta_Psi_alternative, 1,
+                     test.Phi_Theta_Psi_alternative[1]);
+      gsl_vector_set(Phi_Theta_Psi_alternative, 2,
+                     test.Phi_Theta_Psi_alternative[2]);
+      test_numerical_equality(Phi_Theta_Psi_reconstructed,
+                              Phi_Theta_Psi_alternative, epsilon);
+    }
 
-  gsl_vector *y_rotated_to_x = gsl_vector_alloc(3);
-  euler_angle_transform::rotate(y_rotated_to_x, Phi_Theta_Psi_y_to_x, y_axis);
-  test_numerical_equality(3, y_rotated_to_x, x_axis, epsilon);
+    // (5) Verify that the alternative set of Euler angles and the original one
+    // indeed perform the same transformation by applying it to all three
+    // primitive axes.
+    euler_angle_transform::rotate(x_axis_transformed, Phi_Theta_Psi, x_axis);
+    euler_angle_transform::rotate(x_axis_transformed_from_reconstructed_matrix,
+                                  Phi_Theta_Psi_reconstructed, x_axis);
+    test_numerical_equality(x_axis_transformed_from_reconstructed_matrix,
+                            x_axis_transformed, epsilon);
+    euler_angle_transform::rotate(y_axis_transformed, Phi_Theta_Psi, y_axis);
+    euler_angle_transform::rotate(y_axis_transformed_from_reconstructed_matrix,
+                                  Phi_Theta_Psi_reconstructed, y_axis);
+    test_numerical_equality(y_axis_transformed_from_reconstructed_matrix,
+                            y_axis_transformed, epsilon);
+    euler_angle_transform::rotate(z_axis_transformed, Phi_Theta_Psi, z_axis);
+    euler_angle_transform::rotate(z_axis_transformed_from_reconstructed_matrix,
+                                  Phi_Theta_Psi_reconstructed, z_axis);
+    test_numerical_equality(z_axis_transformed_from_reconstructed_matrix,
+                            z_axis_transformed, epsilon);
+  }
 
-  gsl_vector *y_rotated_to_x_reversed = gsl_vector_alloc(3);
-  euler_angle_transform::rotate_back(y_rotated_to_x_reversed,
-                                     Phi_Theta_Psi_y_to_x, y_rotated_to_x);
-  test_numerical_equality(3, y_rotated_to_x_reversed, y_axis, epsilon);
+  gsl_vector_free(x_axis);
+  gsl_vector_free(x_axis_transformed);
+  gsl_vector_free(x_axis_transformed_from_reconstructed_matrix);
+  gsl_vector_free(y_axis);
+  gsl_vector_free(y_axis_transformed);
+  gsl_vector_free(y_axis_transformed_from_reconstructed_matrix);
+  gsl_vector_free(z_axis);
+  gsl_vector_free(z_axis_transformed);
+  gsl_vector_free(z_axis_transformed_from_reconstructed_matrix);
 
-  // Rotate y axis into z axis
-  gsl_vector *Phi_Theta_Psi_y_to_z = gsl_vector_alloc(3);
-  gsl_vector_set_zero(Phi_Theta_Psi_y_to_z);
-  gsl_vector_set(Phi_Theta_Psi_y_to_z, 1, -M_PI_2);
-
-  gsl_vector *y_rotated_to_z = gsl_vector_alloc(3);
-  euler_angle_transform::rotate(y_rotated_to_z, Phi_Theta_Psi_y_to_z, y_axis);
-  test_numerical_equality(3, y_rotated_to_z, z_axis, epsilon);
-
-  gsl_vector *y_rotated_to_z_reversed = gsl_vector_alloc(3);
-  euler_angle_transform::rotate_back(y_rotated_to_z_reversed,
-                                     Phi_Theta_Psi_y_to_z, y_rotated_to_z);
-  test_numerical_equality(3, y_rotated_to_z_reversed, y_axis, epsilon);
-
-  // Rotate z axis into x axis
-  gsl_vector *Phi_Theta_Psi_z_to_x = gsl_vector_alloc(3);
-  gsl_vector_set_zero(Phi_Theta_Psi_z_to_x);
-  gsl_vector_set(Phi_Theta_Psi_z_to_x, 1, M_PI_2);
-  gsl_vector_set(Phi_Theta_Psi_z_to_x, 2, M_PI_2);
-
-  gsl_vector *z_rotated_to_x = gsl_vector_alloc(3);
-  euler_angle_transform::rotate(z_rotated_to_x, Phi_Theta_Psi_z_to_x, z_axis);
-  test_numerical_equality(3, z_rotated_to_x, x_axis, epsilon);
-
-  gsl_vector *z_rotated_to_x_reversed = gsl_vector_alloc(3);
-  euler_angle_transform::rotate_back(z_rotated_to_x_reversed,
-                                     Phi_Theta_Psi_z_to_x, z_rotated_to_x);
-  test_numerical_equality(3, z_rotated_to_x_reversed, z_axis, epsilon);
-
-  // Rotate z axis into y axis
-  gsl_vector *Phi_Theta_Psi_z_to_y = gsl_vector_alloc(3);
-  gsl_vector_set_zero(Phi_Theta_Psi_z_to_y);
-  gsl_vector_set(Phi_Theta_Psi_z_to_y, 1, M_PI_2);
-
-  gsl_vector *z_rotated_to_y = gsl_vector_alloc(3);
-  euler_angle_transform::rotate(z_rotated_to_y, Phi_Theta_Psi_z_to_y, z_axis);
-  test_numerical_equality(3, z_rotated_to_y, y_axis, epsilon);
-
-  gsl_vector *z_rotated_to_y_reversed = gsl_vector_alloc(3);
-  euler_angle_transform::rotate_back(z_rotated_to_y_reversed,
-                                     Phi_Theta_Psi_z_to_y, z_rotated_to_y);
-  test_numerical_equality(3, z_rotated_to_y_reversed, z_axis, epsilon);
+  gsl_vector_free(initial_axis);
+  gsl_vector_free(Phi_Theta_Psi);
+  gsl_vector_free(initial_axis_transformed);
+  gsl_vector_free(initial_axis_reconstructed);
+  gsl_vector_free(target_axis);
+  gsl_matrix_free(transformation_matrix);
+  gsl_vector_free(Phi_Theta_Psi_reconstructed);
+  gsl_vector_free(Phi_Theta_Psi_alternative);
 }
