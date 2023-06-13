4501313e26b863dd503a7c4968110e559bf9daab
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2023-06-13 19:48:51 +0200

    Switch from nanobind to pybind11
    nanobind is the successor of pybind11 and significantly more optimized.
    However, its feature-set is significantly smaller (by design).
    Currently, there is no alternative to the py::vectorize function
    provided by pybind11, which is very useful to easily feed numpy arrays
    into functions. Therefore, I am switching back to pybind11.

--- source/PythonBindings.cc
+++ source/PythonBindings.cc
@@ -1,25 +1,27 @@
-#include <nanobind/nanobind.h>
-#include <nanobind/stl/string.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-// #include <alpaca/AngularCorrelation.hh>
+#include <alpaca/AngularCorrelation.hh>
+// #include <alpaca/EulerAngleRotation.hh>
 // #include <alpaca/CascadeSampler.hh>
 #include <alpaca/State.hh>
 #include <alpaca/Transition.hh>
 
-namespace nb = nanobind;
+namespace py = pybind11;
 using namespace alpaca;
 
-NB_MODULE(alpaca, m) {
-  nb::enum_<EMCharacter>(m, "EMCharacter")
-    .value("electric", EMCharacter::electric)
-    .value("magnetic", EMCharacter::magnetic)
-    .value("unknown", EMCharacter::unknown);
-  nb::class_<Transition>(m, "Transition")
-    .def(nb::init<int, int, double>(), nb::arg("two_L"), nb::arg("two_Lp"),
-         nb::arg("delta") = 0.)
-    .def(nb::init<EMCharacter, int, EMCharacter, int, double>(),
-         nb::arg("em"), nb::arg("two_L"), nb::arg("emp"),
-         nb::arg("two_Lp"), nb::arg("delta") = 0., R"doc(
+PYBIND11_MODULE(alpaca, m) {
+  py::enum_<EMCharacter>(m, "EMCharacter")
+      .value("electric", EMCharacter::electric)
+      .value("magnetic", EMCharacter::magnetic)
+      .value("unknown", EMCharacter::unknown);
+  py::class_<Transition>(m, "Transition")
+      .def(py::init<int, int, double>(), py::arg("two_L"), py::arg("two_Lp"),
+           py::arg("delta") = 0.)
+      .def(py::init<EMCharacter, int, EMCharacter, int, double>(),
+           py::arg("em"), py::arg("two_L"), py::arg("emp"), py::arg("two_Lp"),
+           py::arg("delta") = 0., R"doc(
             Constructor
 
             Parameters
@@ -35,22 +37,25 @@ NB_MODULE(alpaca, m) {
             delta: float
                 Multipole mixing ratio.
          )doc")
-    .def_rw("em_char", &Transition::em_char, "Primary EM character")
-    .def_rw("two_L", &Transition::two_L, "Two times the primary multipolarity")
-    .def_rw("em_charp", &Transition::em_charp, "Secondary EM character")
-    .def_rw("two_Lp", &Transition::two_Lp, "Two times the secondary multipolarity")
-    .def_rw("delta", &Transition::delta, "Multipole mixing ratio")
-    .def("__repr__", &Transition::str_rep);
+      .def_readwrite("em_char", &Transition::em_char, "Primary EM character")
+      .def_readwrite("two_L", &Transition::two_L,
+                     "Two times the primary multipolarity")
+      .def_readwrite("em_charp", &Transition::em_charp,
+                     "Secondary EM character")
+      .def_readwrite("two_Lp", &Transition::two_Lp,
+                     "Two times the secondary multipolarity")
+      .def_readwrite("delta", &Transition::delta, "Multipole mixing ratio")
+      .def("__repr__", &Transition::str_rep);
 
-  nb::enum_<Parity>(m, "Parity")
-    .value("negative", Parity::negative)
-    .value("positive", Parity::positive)
-    .value("unknown", Parity::unknown);
-  nb::class_<State>(m, "State")
-    .def(nb::init<int>(), nb::arg("two_J"))
-    .def(nb::init<int, Parity>(), nb::arg("two_J"), nb::arg("parity"))
-    .def(nb::init<int, Parity, double>(), nb::arg("two_J"), nb::arg("parity"),
-         nb::arg("energy"), R"doc(
+  py::enum_<Parity>(m, "Parity")
+      .value("negative", Parity::negative)
+      .value("positive", Parity::positive)
+      .value("unknown", Parity::unknown);
+  py::class_<State>(m, "State")
+      .def(py::init<int>(), py::arg("two_J"))
+      .def(py::init<int, Parity>(), py::arg("two_J"), py::arg("parity"))
+      .def(py::init<int, Parity, double>(), py::arg("two_J"), py::arg("parity"),
+           py::arg("energy"), R"doc(
             Constructor
 
             Parameters
@@ -62,13 +67,22 @@ NB_MODULE(alpaca, m) {
             energy: float
                 Excitation energy of the state with respect to the ground state in MeV
         )doc")
-    .def(nb::init<int, double>(), nb::arg("two_J"), nb::arg("energy"))
-    .def_rw("two_J", &State::two_J,
-            "Two times the angular momentum quantum number in units of the "
-            "reduced Planck constant")
-    .def_rw("parity", &State::parity, "Parity quantum number")
-    .def_rw("excitation_energy", &State::excitation_energy,
-            "Excitation energy of the state with respect to the ground state "
-            "in MeV")
-    .def("__repr__", &State::str_rep);
+      .def(py::init<int, double>(), py::arg("two_J"), py::arg("energy"))
+      .def_readwrite(
+          "two_J", &State::two_J,
+          "Two times the angular momentum quantum number in units of the "
+          "reduced Planck constant")
+      .def_readwrite("parity", &State::parity, "Parity quantum number")
+      .def_readwrite(
+          "excitation_energy", &State::excitation_energy,
+          "Excitation energy of the state with respect to the ground state "
+          "in MeV")
+      .def("__repr__", &State::str_rep);
+
+  py::class_<AngularCorrelation>(m, "AngularCorrelation")
+      .def(py::init<State, std::vector<std::pair<Transition, State>>>())
+      .def(py::init<State, std::vector<State>>())
+      .def("__call__",
+           py::vectorize(py::overload_cast<const double, const double>(
+               &AngularCorrelation::operator(), py::const_)));
 }
